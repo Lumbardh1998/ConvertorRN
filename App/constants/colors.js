e5a637c8856b438806979ee6e1547d17bd751b61
Lumbardh Sallauka <100045544+Lumbardh1998@users.noreply.github.com>
2022-09-19 15:49:28 +0200

    konvertori

---
+++ App/constants/colors.js
@@ -0,0 +1,9 @@
+export default {
+    text: 'black',
+    border: '#E2E2E2',
+    blue: '#4f7d7a',
+    white: "#fff",
+    textLight: "#797979",
+    offWhite: "#F0F0F0",
+    title: "#000000",
+};
